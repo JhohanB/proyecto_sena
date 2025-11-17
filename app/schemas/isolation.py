9d9c2c3c8b556fd2d50e2f38c70d547fbf295a3a
Jeferson Rios <jefry1795@hotmail.com>
2025-11-17 12:47:56 -0500

    actualizando asilamiento y galpones

--- app/schemas/isolation.py
+++ app/schemas/isolation.py
@@ -8,7 +8,7 @@ class IsolationBase(BaseModel):
     id_galpon: int
 
 class IsolationCreate(IsolationBase):
-    pass
+    fecha_hora: datetime.datetime = Field(..., description="Fecha y hora del aislamiento")
 
 class IsolationUpdate(BaseModel):
     id_incidente_gallina: Optional[int] = None
@@ -19,7 +19,7 @@ class IsolationEstado(BaseModel):
     pass
 
 class IsolationOut(IsolationBase):
-    fecha_hora: datetime.datetime
+    fecha_hora: datetime.datetime = Field(..., description="Fecha y hora del aislamiento")
     id_aislamiento: int
     nombre: str
 
@@ -29,4 +29,5 @@ class PaginatedIsolations(BaseModel):
     total_isolation: int
     total_pages: int
     isolation: List[IsolationOut]
+    
    
